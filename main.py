1d31b1423e5883b3e164f6a5a04c47eb7a2a74f1
rauf.kaya65 <rauf.kaya65@gmail.com>
2025-12-04 12:25:28 +0000

    Changes before Firebase Studio auto-run

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from myapp!")
+import random
+import string
 
+character = int(input("Kaç karakterli olsun?:"))
+specialCharacters = True if input("Özel karakterler olsun mu? (evet/hayır):").lower() == "evet" else False
+numbers = True if input("Sayılar olsun mu? (evet/hayır):").lower() == "evet" else False
 
-if __name__ == "__main__":
-    main()
+password = ""
+tum_karakterler = string.ascii_letters
+if specialCharacters == True: tum_karakterler += string.punctuation
+if numbers == True: tum_karakterler += string.digits
+
+while (len(password) < character): 
+    password = password+random.choice(tum_karakterler)
+
+print(password)
